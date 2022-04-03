e0be863852b755cbca44ab90a394ea8280e7616e
harismateen <sweethari88@gmail.com>
2022-04-03 13:55:16 +0800

    live chat

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,27 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-      </main>
+          </main>
+
+          <body>
+
+              <script>
+                  (function(d,t) {
+        var BASE_URL="https://doc-livechat-2.herokuapp.com";
+        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=BASE_URL+"/packs/js/sdk.js";
+        g.defer = true;
+        g.async = true;
+        s.parentNode.insertBefore(g,s);
+        g.onload=function(){
+                      window.chatwootSDK.run({
+                          websiteToken: 'CNHXeo6MLyXiQMtKB1CAXQXS',
+                          baseUrl: BASE_URL
+                      })
+                  }
+      })(document,"script");
+    </script>
+</body>
 
       <Footer />
     </div>
